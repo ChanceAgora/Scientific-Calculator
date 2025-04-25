ae61bc5985872f4cad9b2532d59c229abc476689
Chance Agorastos <cagorastos127@gmail.com>
2025-04-25 07:58:18 -0500

    Add Checklist
    Add checklist to the top of the main python file for necessary features and implementation plan.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,19 @@
+"""
+Implement Lexer
+    Implement Dispatcher FSM
+    Implement Integer FSM
+    Implement Operator FSM
+    Implement Identifier FSM (for non-operator/noninteger inputs such as sin, cos, log, etc)
+
+Implement Parser
+    Conduct further research as to the elements of a parser
+
+Implement an executor for parsed expression
+    Implement ATS
+    Implement Tree Navigator
+    Ensure Executor's conformity to Arithmetical rules
+"""
+
 import enum
 
 def main():
