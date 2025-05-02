b1977ee0959093900b58b5c45eff5818040f60e6
Chance Agorastos <cagorastos127@gmail.com>
2025-05-02 11:32:44 -0500

    Create FSM File and Refine Pseudocode
    Create the FSM\ Models.py file for the FSM set that will lex the user input.
    
    Alter Pseudo Code to accurately reflect current implementation plans.

--- calculator.py
+++ calculator.py
@@ -1,45 +1,21 @@
-"""
-Implement Lexer
-    Implement Dispatcher FSM
-    Implement Integer FSM
-    Implement Operator FSM
-    Implement Identifier FSM (for non-operator/noninteger inputs such as sin, cos, log, etc)
-
-Implement Parser
-    Conduct further research as to the elements of a parser
-
-Implement an executor for parsed expression
-    Implement ATS
-    Implement Tree Navigator
-    Ensure Executor's conformity to Arithmetical rules
-"""
-
-import enum
-
-def main():
-    x = Token(TokenType.NUMBER, 3.14159)
-    print(x)
-
-class Token:
-    def __init__(self, type_, value=None):
-        self.type = type_
-        self.value = value
-    
-    def __str__(self):
-        return f"{self.type.name}({self.value})"
-        
-class TokenType(enum.Enum):
-    NUMBER = "NUMBER"
-    OPERATOR = "OPERATOR"
-    IDENTIFIER = "IDENTIFIER"
-
-OPERATORS = {
-    "+": lambda a, b: a + b,
-    "-": lambda a, b: a - b,
-    "*": lambda a, b: a * b,
-    "/": lambda a, b: a / b,
-    "^": lambda a, b: a ** b
-}
 
 
-main()
+# Take Input 
+#
+#
+# Lex Input 
+# Use Dispatch FSM to pass value types to subordinate FSMs
+# Use NUMBER, OPERATOR, and IDENTIFIER FSMs to create tokens of their respective types
+#
+#
+#
+# Parse Input
+# Create Nodes based on input precendence and mathematical rules to build an ATS
+#
+#
+#
+# Evaluate the ATS
+#
+#
+#
+# Return value after evaluation
