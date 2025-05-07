b49bf232a2cf7b26449f05e9effe2f4de890fb67
Chance Agorastos <cagorastos127@gmail.com>
2025-05-07 07:29:19 -0500

    Add Basic Notaiton for FSM Methods

--- FSM_Models.py
+++ FSM_Models.py
@@ -1,10 +1,15 @@
 class dispatchFSM:
-    index = 0
-
+    index = 0 # Global FSM class and child class variable to track current position in input string
+    
+    
+    
     def next(self):
+        """Increases the current index by 1 if the current index is not the last index."""
+        #if index < string.size()
         dispatchFSM.index += 1
 
     def error(self):
+        """Outputs the currently known equaiton with an arrow under the index of error."""
         for i in range(dispatchFSM.index):
             print(" ", end="")
         print("^")
