e34791c7a42f6b818d3d338111ea91fb3191283f
Chance Agorastos <cagorastos127@gmail.com>
2025-05-08 02:29:39 -0500

    Add Pseudocode for Lex Method

--- FSM_Models.py
+++ FSM_Models.py
@@ -1,24 +1,36 @@
-class dispatchFSM:
+class DispatchFSM:
     index = 0 # Global FSM class and child class variable to track current position in input string
     
-    
+    def lexInput(equation) -> list:
+        """(Not yet Implemented)\n
+        Takes a string input and returns a list of all valid tokens from that string.\n
+        Returns an error if there are invalid inputs present."""
+
+        tokens = []
+        
+        # Loop until last index is evaluated
+            # Check current character
+            # Pas int to NumberFSM
+            # Pass operation symbol to OperatorFSM
+            # Pass anything else to IdentifierFSM
+
     
     def next(self):
         """Increases the current index by 1 if the current index is not the last index."""
         #if index < string.size()
-        dispatchFSM.index += 1
+        DispatchFSM.index += 1
 
     def error(self):
         """Outputs the currently known equaiton with an arrow under the index of error."""
-        for i in range(dispatchFSM.index):
+        for i in range(DispatchFSM.index):
             print(" ", end="")
         print("^")
 
-class numberFSM(dispatchFSM):
+class NumberFSM(DispatchFSM):
     pass
 
-class operatorFSM(dispatchFSM):
+class OperatorFSM(DispatchFSM):
     pass
 
-class IdentifierFSM(dispatchFSM):
+class IdentifierFSM(DispatchFSM):
     pass
